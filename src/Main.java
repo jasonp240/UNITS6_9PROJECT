c8ddc4bb768354e7fb6ddadac625cc87469628b9
BT_1N3_22 <sample@bths.edu>
2024-03-15 09:18:03 -0400

    finished move and also created maps 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+        Map i = new Map();
+        i.setupMap();
+        i.printMap();
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.move("W");
+        i.printMap();
+    }
 }
--- src/Map.java
+++ src/Map.java
@@ -4,6 +4,99 @@ public class Map {
     private Space walkSpace;
     private Space lootSpace;
     private Space treeSpace;
+    private Space[][] curMap;
+    private int curMapNum;
+    private int playerX;
+    private int playerY;
+    private Space prev;
+
+    public Map() {
+        playerSpace = new Space("\uD83E\uDDD1");
+        grassSpace = new Space("\uD83D\uDFE9");
+        walkSpace = new Space("\uD83D\uDFEB");
+        lootSpace = new Space("\uD83D\uDCE6");
+        treeSpace = new Space("\uD83C\uDF33");
+        curMapNum = 0;
+        prev = walkSpace;
+    }
+
+    public void setupMap() {
+        playerX = 4;
+        playerY = 9;
+          curMap = new Space[][]{
+                  {lootSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, treeSpace, treeSpace},
+                  {grassSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, lootSpace},
+                  {grassSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {grassSpace, grassSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {treeSpace, grassSpace, grassSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {treeSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {treeSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {treeSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, treeSpace, grassSpace, treeSpace},
+                  {treeSpace, treeSpace, treeSpace, walkSpace, walkSpace, walkSpace, grassSpace, grassSpace, treeSpace},
+                  {treeSpace, treeSpace, treeSpace, walkSpace, playerSpace, walkSpace, treeSpace, treeSpace, treeSpace}
+        };
+    }
+
+    public void printMap() {
+        for (Space[] row : curMap) {
+            for (Space element : row) {
+                System.out.print(element.getSymbol());
+            }
+            System.out.println();
+        }
+    }
+
+    public void nextMap() {
+        curMapNum++;
+        if (curMapNum == 1) {
+            curMap = new Space[][]{
+                    {treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace},
+                    {treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace},
+                    {treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace, treeSpace},
+                    {treeSpace, grassSpace, grassSpace, grassSpace, grassSpace, grassSpace, grassSpace, treeSpace, treeSpace},
+                    {treeSpace, grassSpace, treeSpace, treeSpace, treeSpace, treeSpace, grassSpace, treeSpace, treeSpace},
+                    {treeSpace, grassSpace, treeSpace, treeSpace, treeSpace, treeSpace, grassSpace, treeSpace, treeSpace},
+                    {treeSpace, grassSpace, treeSpace, treeSpace, treeSpace, treeSpace, grassSpace, treeSpace, treeSpace},
+                    {treeSpace, grassSpace, grassSpace, grassSpace, grassSpace, treeSpace, grassSpace, treeSpace, treeSpace},
+                    {treeSpace, treeSpace, treeSpace, treeSpace, grassSpace, treeSpace, treeSpace, treeSpace, treeSpace},
+                    {treeSpace, treeSpace, treeSpace, treeSpace, playerSpace, treeSpace, treeSpace, treeSpace, treeSpace}
+            };
+            prev = grassSpace;
+        }
+    }
+
+    public void move(String input) {
+
+        if (input.equals("W")){
+            if (playerY == 0) {
+                playerX = 4;
+                playerY = 9;
+                nextMap();
+            } else {
+                curMap[playerY][playerX] = prev;
+                prev = curMap[playerY - 1][playerX];
+                curMap[playerY - 1][playerX] = playerSpace;
+                playerY--;
+            }
+        } else if (input.equals("S") && playerY < 9) {
+            curMap[playerY][playerX] = prev;
+            prev = curMap[playerY + 1][playerX];
+            curMap[playerY + 1][playerX] = playerSpace;
+            playerY++;
+        } else if (input.equals("D") && playerX < 7 ) {
+            curMap[playerY][playerX] = prev;
+            prev = curMap[playerY][playerX + 1];
+            curMap[playerY][playerX + 1] = playerSpace;
+            playerX++;
+        } else if (input.equals("A") && playerX > 0) {
+            curMap[playerY][playerX] = prev;
+            prev = curMap[playerY][playerX - 1];
+            curMap[playerY][playerX - 1] = playerSpace;
+            playerX--;
+        } else {
+            System.out.println("You will go out of bound!");
+        }
+    }
 
 
 }
