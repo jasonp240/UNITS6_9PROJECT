a564a93cc2c8c6ef14f4d2c34595c88404c264de
BT_1N3_22 <sample@bths.edu>
2024-03-22 09:13:13 -0400

    finished sword, armour, and potion classes

--- src/Armour.java
+++ src/Armour.java
@@ -1,2 +1,16 @@
 public class Armour extends Loot {
+    private int upgrade;
+    public Armour(String name, int durability) {
+        super(name, durability);
+        upgrade = 0;
+    }
+
+    public boolean upgrade() {
+        if (upgrade < 10) {
+            return false;
+        } else {
+            upgrade++;
+            return true;
+        }
+    }
 }
--- src/Sword.java
+++ src/Sword.java
@@ -1,2 +1,16 @@
 public class Sword extends Loot {
+    private int upgrade;
+    public Sword(String name, int durability) {
+        super(name, durability);
+        upgrade = 0;
+    }
+
+    public boolean upgrade() {
+        if (upgrade < 10) {
+            return false;
+        } else {
+            upgrade++;
+            return true;
+        }
+    }
 }
